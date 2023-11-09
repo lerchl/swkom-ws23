6c91ff4d713cca417790f8acc99dd505b84d953d
TORz69 <91666614+TORz69@users.noreply.github.com>
2023-11-09 12:29:28 +0100

    Update LoggerFactory.cs

--- rest/rest.logging/LoggerFactory.cs
+++ rest/rest.logging/LoggerFactory.cs
@@ -3,7 +3,7 @@
 namespace Rest.Logging {
     public class LoggerFactory {
 
-        public static ILoggerWrapper GetLogger() {
+        public static ILogger GetLogger() {
             StackTrace stackTrace = new StackTrace(1, false); //Captures 1 frame, false for not collecting information about the file
             var type = stackTrace.GetFrame(1).GetMethod().DeclaringType;
             return LoggerWrapper.CreateLogger("./log4net.config", type.FullName);
