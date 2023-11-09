d1a3103bc5edaa7fdc1c23d323014292b0048fe1
TORz69 <91666614+TORz69@users.noreply.github.com>
2023-11-09 12:38:12 +0100

    Fix (Maybe)

--- rest/rest.logging/LoggerFactory.cs
+++ rest/rest.logging/LoggerFactory.cs
@@ -2,7 +2,6 @@
 
 namespace Rest.Logging {
     public class LoggerFactory {
-
         public static ILogger GetLogger() {
             StackTrace stackTrace = new StackTrace(1, false); //Captures 1 frame, false for not collecting information about the file
             var type = stackTrace.GetFrame(1).GetMethod().DeclaringType;
