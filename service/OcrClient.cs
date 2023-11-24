502febb94e035409bc956e2ffd5c7baa3b966af3
Nico Lerchl <nico@lerchl.one>
2023-11-24 19:19:40 +0100

    ocr worker prints to console

--- service/OcrClient.cs
+++ service/OcrClient.cs
@@ -7,15 +7,6 @@ namespace service;
 
 public class OcrClient
 {
-    private readonly string tessDataPath;
-    private readonly string language;
-
-    public OcrClient()
-    {
-        this.tessDataPath = "./tessdata";
-        this.language = "en";
-    }
-
     public string OcrPdf(Stream pdfStream)
     {
         var stringBuilder = new StringBuilder();
@@ -30,7 +21,7 @@ public class OcrClient
                 magickImage.Format = MagickFormat.Png;
 
                 // Perform OCR on the image
-                using (var tesseractEngine = new TesseractEngine(tessDataPath, language, EngineMode.Default))
+                using (var tesseractEngine = new TesseractEngine("tessdata", "eng", EngineMode.Default))
                 {
                     using (var page = tesseractEngine.Process(Pix.LoadFromMemory(magickImage.ToByteArray())))
                     {
